6100f77226b3cfbb875504d2edbdd4fd394efe6c
Tom Harrop <twharrop@gmail.com>
2019-09-04 12:32:37 +1200

    plot map

--- src/plot_map.R
+++ src/plot_map.R
@@ -58,7 +58,7 @@ gp <- ggplot(wasploc[!is.na(Range)],
     xlab(NULL) + ylab(NULL) +
     coord_quickmap() +
     facet_wrap(~ Range, ncol = 1, strip.position = "left") +
-    scale_colour_brewer(palette = "Dark2") +
+    scale_colour_viridis_d() +
     scale_shape_manual(values = c(16, 17), guide = FALSE) +
     guides(colour = guide_legend(title = NULL,
                                  override.aes = list(size = 2,
@@ -68,7 +68,7 @@ gp <- ggplot(wasploc[!is.na(Range)],
                size = 1)
 
 
-saveRDS(gp, "gp.Rds")
+saveRDS(gp, "output/gp.Rds")
 
 PtToMm <- function(x){
     grid::convertUnit(grid::unit(x, "pt"), "mm", valueOnly = TRUE)
@@ -76,7 +76,7 @@ PtToMm <- function(x){
 
 
 
-ggsave("vespula_worldmap.pdf",
+ggsave("figs/vespula_worldmap.pdf",
        gp,
        device = cairo_pdf(),
        width = PtToMm(398),
